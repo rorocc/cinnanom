1aa6f311052cb1b2ac20aae9ceccf0737a64255e
crock <christopher.rock@student.uni-luebeck.de>
2022-10-27 00:09:38 +0200

    setup

---
+++ components/cardSmall.vue
@@ -0,0 +1,38 @@
+<template>
+ <div class="card w-full flex">
+   <div class="cardImg w-1/4" />
+   <div class="w-full relative w-3/4">
+     <p>{{ bakery }}</p>
+     <p>{{ price }}</p>
+     <p>{{ sweetness }}</p>
+     <img src="/cardbottom.svg" class="w-full h-auto bottom-0 left-0 absolute" />
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "cardSmall",
+  props: {
+    bakery: String,
+    price: Number,
+    sweetness: Number
+  }
+}
+</script>
+
+<style scoped>
+  .card{
+    background-color: #F8D5B6;
+    overflow: hidden;
+    @apply rounded-2xl;
+  }
+
+  .cardImg{
+    border-right: 2px solid #FFF3E8;
+    background-image: url("/img/example.jpeg");
+    background-size: cover;
+    background-position: center;
+    @apply w-48 h-48;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <section class="head">
+      <h1>cinnanom</h1>
+      <h2>Discover the world of cinnamon buns in Oslo.</h2>
+    </section>
+    <img src="/divider.svg" class="w-full">
+    <section>
+      <h1 class="gradient padding"><span>The greatest bun</span></h1>
+    </section>
+    <img src="/divider.svg" class="w-full">
+    <section>
+      <h1 class="gradient padding"><span>Take a look at all the buns</span></h1>
+      <div class="grid md:grid-cols-2 grid-cols-1 space-x-4 space-y-4">
+        <card-small v-for="rating in data"
+                    :bakery="rating.bakery"
+                    :price="rating.price"
+                    :sweetness="rating.sweetness" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import ratingsData from "../static/data.json"
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      data: ratingsData
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+
+  body{
+    background-color: #FFEAD7;
+  }
+
+  section{
+    @apply container mx-auto text-center;
+  }
+
+  h1{
+    font-family: 'Playfair Display', serif;
+    color: #724012;
+    @apply text-4xl p-16;
+  }
+
+  h1.gradient span{
+    background: linear-gradient(0deg, rgba(255,0,255,0) 0%, rgba(255,0,255,0) 16%, rgb(248, 213, 182) 16%, rgb(248, 213, 182) 41%, rgba(255,0,255,0) 41%);
+  }
+
+  h1.padding span{
+    padding: 0.5em 0.2em 0.5em 0.2em;
+    box-decoration-break: clone;
+    -webkit-box-decoration-break: clone;
+  }
+
+  .head{
+    background-color: #F8D5B6;
+  }
+</style>
